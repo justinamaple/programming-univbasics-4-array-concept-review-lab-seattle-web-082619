c55e978fe84289c0c4a68708182f1b3656f453b4
Justin Maple <justinamaple@gmail.com>
2019-08-18 18:48:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -16,5 +16,9 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  min = Integer::MAX
+  
+  array.each do |integer|
+    min = integer < min ? integer : max
+  end
 end
