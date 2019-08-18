9dab9a4403653d4e6bed9edbfbbfb0a68ddc4400
Justin Maple <justinamaple@gmail.com>
2019-08-18 18:55:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,16 +9,12 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  max = Integer::MIN
-  
   array.each do |integer|
     max = integer > max ? integer : max
   end
 end
 
 def find_min_value(array)
-  min = Integer::MAX
-  
   array.each do |integer|
     min = integer < min ? integer : min
   end
